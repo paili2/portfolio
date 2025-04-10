0a428ec678ad783178a2956dfbac05477d67d268
paili2 <zxcv7502@naver.com>
2025-04-10 14:07:13 +0900

    　

--- src/features/main/project/CardSlide.jsx
+++ src/features/main/project/CardSlide.jsx
@@ -62,7 +62,7 @@ const CardSlide = () => {
         skil2={<SkilTag skil={"CSS"} bg={"rgb(192, 214, 255)"} />}
         skil3={<SkilTag skil={"Javascript"} bg={"rgb(195, 255, 192)"} />}
         skil4={<SkilTag skil={"React"} bg={"rgb(255, 240, 192)"} />}
-        link={""}
+        link={"https://paili2.github.io/musinsa-clone/"}
       ></ProjectCard>
       {/* <button className={styles.slide_btn}>
           <FaArrowRight />
